04addb75c57cfc126c0c93b176fbba2ed129ecbe
Kis Attila Balint <balint.attila.kis@balabit.com>
2020-02-17 16:34:34 +0100

    Create dir if not exists

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -8,9 +8,11 @@ fi
 
 if ! [ -x "$(command -v git)" ]; then
     pacman -Sy git
-if
+fi
 
-[ -d "~/Desktop/projects/tools/manage-me" ] && git clone "https://github.com/atosz33/manage-me.git" "~/Desktop/projects/tools"
+[ -d "~/Desktop/projects/tools/manage-me" ] && \
+    mkdir -p ~/Desktop/projects/tools/      && \
+    git clone "https://github.com/atosz33/manage-me.git" "~/Desktop/projects/tools"
 
 cd ~/Desktop/projects/tools/manage-me
 
